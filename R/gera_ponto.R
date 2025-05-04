09426c59d4b8493ea50db8614bef1d194c416167
João Victor Siqueira Rodrigues <jvsiqueira2013@gmail.com>
2025-05-04 18:37:27 -0300

    gera ponto

--- R/gera_ponto.R
+++ R/gera_ponto.R
@@ -5,17 +5,26 @@
 #' @importFrom wavethresh GenW
 #' @importFrom wavethresh accessC
 #'
-#' @param n Quantidade de pontos por função.
-#' @param y Dados.
-#' @param lim_sup Valor máximo gerado para o ponto gerado a.
+#' @param y Valores observados.
+#' @param lim_sup Valor máximo para uma coordenada gerado para o ponto gerado a.
 #' @inheritParams wavethresh::wd
+#'
+#' @description
+#' Gera um ponto no domínio do tempo da função original garantindo que esta
+#' dentro do suporte da posteriori com erro Gamma [post_gamma()].
+#'
+#' @examples
+#' y <- c(1,2,7,2,5,6,0,3)  # valores observados
+#' d <- wd(y, filter.number = 5, family = 'DaubExPhase')
+#' (theta <- gera_ponto(y))
+#' post_gamma(theta, d, beta = 5, lambda = 1, tau = 5, alpha = 0.7)  # posteriori
 
-gera_ponto <- function(n, y, lim_sup = 20, filter.number = 5,
+gera_ponto <- function(y, lim_sup = 20, filter.number = 5,
                        family = 'DaubExPhase') {
   dwt <- wd(y, filter.number=filter.number, family=family)
-  d <- c(accessC(dwt, lev=0), dwt$D)  # coeficientes empíricos
-  a <- runif(n, 0.01, lim_sup)        # W(d - theta) = a >= 0
-  W <- t(GenW(n, filter.number=filter.number, family=family))
+  d <- c(accessC(dwt, lev=0), dwt$D)          # coeficientes empíricos
+  a <- runif(length(y), 0.01, lim_sup)        # W(d - theta) = a >= 0
+  W <- t(GenW(length(y), filter.number=filter.number, family=family))
   theta <- d - W %*% a
   return(theta)
 }
