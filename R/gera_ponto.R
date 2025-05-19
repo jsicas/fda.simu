ca21fc90008b3da758843e55bbb66b39429c17f3
João Victor Siqueira Rodrigues <jvsiqueira2013@gmail.com>
2025-05-19 00:56:14 -0300

    modificando ram_gamma

--- R/gera_ponto.R
+++ R/gera_ponto.R
@@ -4,37 +4,56 @@
 #'
 #' @importFrom wavethresh GenW
 #' @importFrom wavethresh accessC
+#' @importFrom wavethresh nlevelsWT
 #'
-#' @param y Valores observados.
+#' @param a vetor com todas as entradas positivas. Se `NULL` é gerado
+#'   aleatóriamente.
 #' @param d Coeficientes empíricos de ondaletas.
 #' @param lim_sup Valor máximo para uma coordenada gerado para o ponto gerado a.
 #' @inheritParams wavethresh::wd
 #'
-#' @description
-#' Gera um ponto no domínio do tempo da função original garantindo que esta
-#' dentro do suporte da posteriori com erro Gamma, ver [post_gamma()].
+#' @description Gera um ponto no domínio do tempo da função original garantindo
+#'   que esta dentro do suporte da posteriori com erro Gamma, ver função
+#'   [post_gamma].
 #'
 #' @examples
+#' # library(wavethresh)
 #' y <- c(1,2,7,2,5,6,0,3)  # valores observados
 #' d <- wd(y, filter.number = 5, family = 'DaubExPhase')
-#' (theta <- gera_ponto(y))
+#' (theta <- gera_ponto(a = NULL, d))
 #' post_gamma(theta, d, beta = 5, lambda = 1, tau = 4, alpha = 0.7)  # posteriori
 
-gera_ponto <- function(y = NULL, d = NULL, lim_sup = 20, filter.number = 5,
+gera_ponto <- function(a = NULL, d, lim_sup = 20, filter.number = 5,
                        family = 'DaubExPhase') {
-  if (!is.null(y) & !is.null(d)) stop('Especifique apenas y ou d.')
-  if (is.null(d)) {
-    dwt <- wd(y, filter.number=filter.number, family=family)
-    d <- c(accessC(dwt, lev=0), dwt$D)  # coeficientes empíricos
-    M <- length(y)  # quantidade de pontos
-  } else if (class(d) != 'wd'){
-    M <- length(d)
-  } else {
-    d <- c(accessC(d, lev=0), d$D)
-  }
-  a <- runif(M, 0.01, lim_sup)  # W(d - theta) = a >= 0
-  W <- t(GenW(M, filter.number=filter.number, family=family))
-  theta <- d - W %*% a
-  return(theta)
+  M <- 2^nlevelsWT(d)
+  W <- t(GenW(M, filter.number, family))
+  if (is.null(a)) a <- runif(M, 0.01, lim_sup)
+  d_emp <- c(accessC(d, lev=0), d$D)  # coeficientes empíricos
+  return(d_emp - W %*% a)  # theta
 }
 
+
+# @param y Valores observados.
+# @examples
+# y <- c(1,2,7,2,5,6,0,3)  # valores observados
+# d <- wd(y, filter.number = 5, family = 'DaubExPhase')
+# (theta <- gera_ponto(y))
+# post_gamma(theta, d, beta = 5, lambda = 1, tau = 4, alpha = 0.7)  # posteriori
+
+# gera_ponto <- function(y = NULL, d = NULL, lim_sup = 20, filter.number = 5,
+#                        family = 'DaubExPhase') {
+#   if (!is.null(y) & !is.null(d)) stop('Especifique apenas y ou d.')
+#   if (is.null(d)) {
+#     dwt <- wd(y, filter.number=filter.number, family=family)
+#     d <- c(accessC(dwt, lev=0), dwt$D)  # coeficientes empíricos
+#     M <- length(y)  # quantidade de pontos
+#   } else if (class(d) != 'wd'){
+#     M <- length(d)
+#   } else {
+#     d <- c(accessC(d, lev=0), d$D)
+#   }
+#   a <- runif(M, 0.01, lim_sup)  # W(d - theta) = a >= 0
+#   W <- t(GenW(M, filter.number=filter.number, family=family))
+#   theta <- d - W %*% a
+#   return(theta)
+# }
--- R/ram_gamma.R
+++ R/ram_gamma.R
@@ -6,10 +6,11 @@
 #' @export
 #'
 #' @param theta_1 Chute inicial para \eqn{\theta}. Se não for definido, será
-#'   gerado um ponto aleatório baseado na função `gera_ponto`.
+#'   gerado um ponto aleatório baseado na função [gera_ponto].
 #' @param S_1 Chute inicial para a matriz S. Se não for definida será utilziado
-#'   a identidaide.
-#' @param f Valores verdadeiros.
+#'   a matriz identidaide.
+#' @param d coeficientes empíricos de ondaleta. Objeto da classe `wd`, ver
+#'   função [wavethresh::wd].
 #' @param alpha Parâmetro da mistura da priori spike and slab utilizada.
 #' @param tau Parâmetro da logística presente na priori.
 #' @param beta,lambda parâmetros do erro \eqn{Gamma(\beta, \lambda)}.
@@ -17,9 +18,8 @@
 #' @param gamma Parâmetro do algorítimo RAM.
 #' @inheritParams wavethresh::wd
 #'
-#' @references
-#' Vihola, M. (2012). Robust adaptive Metropolis algorithm with coerced
-#' acceptance rate. Stat Comput 22, 997–1008. DOI:
+#' @references Vihola, M. (2012). Robust adaptive Metropolis algorithm with
+#' coerced acceptance rate. Stat Comput 22, 997–1008. DOI:
 #' [10.1007/s11222-011-9269-5](https://doi.org/10.1007/s11222-011-9269-5).
 #'
 #' Sousa, A.R.S., Garcia, N.L. (2023). Wavelet shrinkage in
@@ -28,43 +28,37 @@
 #' [10.1080/00949655.2023.2215372](https://doi.org/10.1080/00949655.2023.2215372).
 #'
 #' @examples
-#' sample <- ram_gamma(f = f_test(n = 8)$bumps, beta = 10, lambda = 30, n_ite = 3000)
-#' colMeans(sample$theta)
 
-ram_gamma <- function(theta_1 = NULL, S_1 = NULL, f, alpha=0.8, tau=2,
-                      beta, lambda, n_ite = 50000, gamma = 2/3,
-                      filter.number=5, family='DaubExPhase') {
+ram_gamma <- function(theta_1 = NULL, S_1 = NULL, d, n_ite = 50000, alpha = 0.8,
+                      tau = 2, beta, lambda, gamma = 2/3,
+                      filter.number = 5, family = 'DaubExPhase') {
   # criando objetos
-  n <- length(f)                  # quantidade de pontos por função
-  theta <- matrix(0, n_ite, n)    # matriz contendo amostra de theta
+  M <- length(f)                  # quantidade de pontos por função
+  theta <- matrix(0, n_ite, M)    # matriz contendo amostra de theta
   S_l <- vector(mode='list', length=n_ite)      # lista para armazenar S_l
   gamma_l <- vector(mode='numeric', n_ite - 1)  # vetor para armazenar gamma_l
-
-  # gerando amostra
-  y <- f + rgamma(n, shape=beta, rate=lambda)  # gerando valores observados
-  dwt <- wd(y, filter.number=filter.number, family=family)  # coeficientes empíricos
+  eta <- seq(1, n_ite)^(-gamma)    # parâmetro do algorítimo RAM
 
   # definindo alguns parâmetors
-  if (is.null(S_1)) S_1 <- diag(n)  # chute inicial para matriz S caso não definida
-  if (is.null(theta_1)) theta_1 <- gera_ponto(n, y)
-  theta[1,] <- theta_1             # chute inicial
-  S_l[[1]] <- S_1                  # chute inicial
-  eta <- seq(1, n_ite)^(-gamma)    # parâmetro do algorítimo RAM
+  theta[1,] <- theta_1              # chute inicial
+  S_l[[1]] <- S_1                   # chute inicial
+  if (is.null(S_1)) S_1 <- diag(M)  # chute inicial se S não definido
+  if (is.null(theta_1)) theta_1 <- gera_ponto(a = NULL, d)  # chute inicial se theta_1 não definido
 
   for (i in 2:n_ite) {if (i %% 10000 == 0) message(i)
     # proposta
-    U_l <- t(rmvnorm(1, rep(0, n), diag(n)))
+    U_l <- t(rmvnorm(1, rep(0, M), diag(M)))
     theta_star <- t(theta[i-1,] + S_l[[i-1]] %*% U_l)
 
     # taxa de aceitação
-    gamma_l[i-1] <- min(1, post_gamma(theta_star, dwt, beta, tau, lambda, alpha)/
-                          post_gamma(theta[i-1,], dwt, beta, tau, lambda, alpha))
+    gamma_l[i-1] <- min(1, post_gamma(theta_star, d, beta, tau, lambda, alpha)/
+                          post_gamma(theta[i-1,], d, beta, tau, lambda, alpha))
 
     if (rbinom(1, 1, gamma_l[i-1]) == 1) theta[i,] <- theta_star
     else theta[i,] <- theta[i-1,]
 
     # atualizando S_l
-    A <- S_l[[i-1]] %*% (diag(n) + eta[i]*(gamma_l[i-1] - gamma) *
+    A <- S_l[[i-1]] %*% (diag(M) + eta[i]*(gamma_l[i-1] - gamma) *
                            U_l %*% t(U_l) / as.vector(t(U_l) %*% U_l)) %*% t(S_l[[i-1]])
     S_l[[i]] <- t(chol(A))
   }
