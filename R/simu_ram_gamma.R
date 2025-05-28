068c369b07b7f0bc955bf32142956fabeb0e4e2c
João Victor Siqueira Rodrigues <jvsiqueira2013@gmail.com>
2025-05-28 01:12:05 -0300

    nada novo

--- R/simu_ram_gamma.R
+++ R/simu_ram_gamma.R
@@ -1,6 +1,7 @@
 #' @title Função de Simulação para Dados Funcionais com Erro Gamma
 #'
 #' @importFrom wavethresh wd
+#' @importFrom wavethresh GenW
 #' @importFrom doFuture %dofuture%
 #'
 #' @export
@@ -8,20 +9,23 @@
 #' @param alpha_comp Matriz com as funções componentes, cada coluna é uma
 #'   função.
 #' @param I Tamanho da amostra gerada, isto é, quantidade de observações.
-#' @param bt Vetor contendo os índices de burn-in e thinning.
 #' @inheritParams ram_gamma
+#' @param bt Vetor contendo os índices de burn-in e thinning. Se não for
+#'   especificado será utilizado uma queima de 5% das observações e um thinning
+#'   de 3 observações.
 
-simu_ram_gamma <- function(alpha_comp, I, n_ite, bt, beta, lambda, alpha, tau,
+simu_ram_gamma <- function(alpha_comp, I, n_ite, beta, lambda, alpha, tau,
                            gamma=2/3, filter.number=5, family='DaubExPhase',
-                           lim_sup=15) {
+                           lim_sup=15, bt=NULL) {
   # geral
   M <- nrow(alpha_comp)  # quantidade de pontos por função
   L <- ncol(alpha_comp)  # número de curvas componentes
+  if (is.null(bt)) bt <- seq(round(0.05 * n_ite), n_ite, 3)
 
   # gerando amostra
   y <- apply(matrix(runif(L*I), nrow=L), 2, \(col) col/sum(col))  # pesos
   f <- alpha_comp %*% y
-  e <- matrix(rgamma(M * I, shape=beta, rate=lambda), ncol=I)  # erro
+  e <- matrix(rgamma(M*I, shape=beta, rate=lambda), ncol=I)     # erro
   A <- f + e  # amostra
 
   # DWT
@@ -48,5 +52,7 @@ simu_ram_gamma <- function(alpha_comp, I, n_ite, bt, beta, lambda, alpha, tau,
 
   # IDWT
   alpha_hat <- GenW(M, filter.number, family) %*% delta_D  # curvas estimadas
+
+  # calculando erro
 
 }
